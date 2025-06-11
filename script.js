75f0fa3fa6512c9af975756fc708c7db42824249
Kim Burmester <Kim.Burmester@mailbox.org>
2025-06-11 18:50:19 +0200

    style: table styled.

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ let fields = [
     null,
     null,
 ]
+
+function init() {
+    render();
+}
+function render() {
+  const containerDiv = document.getElementById('container');
+
+  let tableHtml = '<table>';
+  for(let i = 0; i < 3; i++){
+    tableHtml += '<tr>';
+    for(let j = 0; j < 3; j++){
+        const index = i * 3 + j; //NOTE: Berechnet das Feld und deren Index
+        let symbol = '';
+        if(fields[index] === 'circle'){
+            symbol = 'o';
+        }else if(fields[index] === 'cross'){
+            symbol = 'x';
+        }
+        tableHtml += `<td>${symbol}</td>`;
+    }
+    tableHtml += '</tr>';
+  }
+  tableHtml += '</table>';
+
+  containerDiv.innerHTML = tableHtml;
+}
